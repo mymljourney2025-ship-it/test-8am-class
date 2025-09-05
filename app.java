23e13970c3e76e95381d15cb0397a0bc8eb3acc8
DevopsguruRR <rakeshdevopstrainer1@gmail.com>
2025-09-05 08:35:56 +0530

    This is a sample file created for devops 8 am batch

---
+++ app.java
@@ -0,0 +1,17 @@
+public class App {
+	public static void main(String[] args) {
+		System.out.println("====================================");
+		System.out.println("   Welcome to RR Technosoft Institute   ");
+		System.out.println("====================================");
+		System.out.println("Empowering your IT career with quality training!");
+		System.out.println();
+		System.out.println("Courses Offered:");
+		System.out.println("- Java Full Stack");
+		System.out.println("- Python");
+		System.out.println("- Data Science");
+		System.out.println("- AWS & DevOps");
+		System.out.println("- Testing Tools");
+		System.out.println();
+		System.out.println("Contact Us: +91-XXXXXXXXXX | www.rrtechnosoft.com");
+	}
+}
